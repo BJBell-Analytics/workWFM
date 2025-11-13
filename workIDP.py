4ab5241c13bf3716b9e819d6add74c3b54dccc71
Brian Bell <bjbell@slogworx.com>
2025-11-13 12:10:36 -0600

    Added: xlwb_csvs(infile, create_files=True)
    xlwb_csvs(infile)
            -split excel workbook tabs into individual csv files
            -also returns the dictionary of pandas objects
            -set create_files=False to bypass creating files

--- workIDP.py
+++ workIDP.py
@@ -6,6 +6,16 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
+def update_idpdb(daily_idp, database_file, table_name):
+    """update_idpdb(daily_idp, database_file, table_name)
+        -insert daily_idp into the sql database database_file in table_name
+        -db_file is str in 'sqlite:///name.db'
+    """
+    db_engine = create_engine(database_file)
+    daily_idp.to_sql(
+            table_name, con=db_engine, if_exists='replace', index=False)
+
+
 def cleanday(inputfile, idp_date):
     """cleanday(inputfile, idp_date)
         - clean-up raw csv data for a single day from inputfile
@@ -30,11 +40,15 @@ def cleanday(inputfile, idp_date):
     return clean_idp
 
 
-def update_idpdb(daily_idp, database_file, table_name):
-    """update_idpdb(daily_idp, database_file, table_name)
-        -insert daily_idp into the sql database database_file in table_name
-        -db_file is str in 'sqlite:///name.db'
+def xlwb_csvs(infile, create_files=True):
+    """xlwb_csvs(infile)
+        -split excel workbook tabs into individual csv files
+        -also returns the dictionary of pandas objects
+        -set create_files=False to bypass creating files
     """
-    db_engine = create_engine(database_file)
-    daily_idp.to_sql(
-            table_name, con=db_engine, if_exists='replace', index=False)
+    dict_pdobj = pd.read_excel(infile, sheet_name=None)
+    if create_files:
+        for tabdate in dict_pdobj.keys():
+            dict_pdobj[tabdate].to_csv(f'IDP_{tabdate}.csv', index=False)
+
+    return dict_pdobj
