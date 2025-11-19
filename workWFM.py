d8fbe2c1ffd8c0f642a90b148bd311aa6ac5dfdf
Brian Bell <bjbell@slogworx.com>
2025-11-19 13:50:03 -0600

    Minor changers for project organization
    Minor tweaks and renaming. Added cleanSEG() boilerplate code.

--- workWFM.py
+++ workWFM.py
@@ -1,9 +1,12 @@
-"""workIDP - Routines to help track IDP data and aid data exploration
+"""workWFM
+    - Routines to help clean, analyze, and track data for my workforce duties.
 
     Daily data captured from Alvaria "Aspect Workforce"
 """
 import pandas as pd
 from sqlalchemy import create_engine
+
+# functions to convert data formats
 
 
 def update_idpdb(daily_idp, database_file, table_name):
@@ -13,11 +16,27 @@ def update_idpdb(daily_idp, database_file, table_name):
     """
     db_engine = create_engine(database_file)
     daily_idp.to_sql(
-            table_name, con=db_engine, if_exists='replace', index=False)
+            table_name, con=db_engine, if_exists='append', index=False)
 
 
-def cleanday(inputfile, idp_date):
-    """cleanday(inputfile, idp_date)
+def xlwb_csvs(infile, create_files=False):
+    """xlwb_csvs(infile)
+        -extract data from excel workbook tabs (tab label YYY-MM-DD)
+        -returns the dictionary of pandas objects
+        -set create_files=True to create individual csv files
+    """
+    dict_pdobj = pd.read_excel(infile, sheet_name=None)
+    if create_files:  # currently False by default
+        for tabdate in dict_pdobj.keys():
+            dict_pdobj[tabdate].to_csv(f'IDP_{tabdate}.csv', index=False)
+
+    return dict_pdobj
+
+# Cleaning functions
+
+
+def cleanIDP(inputfile, idp_date):
+    """cleanIDP(inputfile, idp_date)
         - clean-up raw csv data for a single day from inputfile
         - add idp_date (type str formatted 'YYYY-MM-DD')
         - convert 'Time Period' to datetime values
@@ -31,24 +50,16 @@ def cleanday(inputfile, idp_date):
                                     'HC Required for SL', 'NET Butts in Seats',
                                     'SL-ACD', 'OAHT', 'A-AHT', 'A-ASA']]
 
-    clean_idp.insert(0, 'Date', pd.to_datetime(idp_date))
-    clean_idp['Date'] = pd.to_datetime(clean_idp['Date'],
-                                       format='%Y-%m-%d').dt.date
+    clean_idp.insert(0, 'Date', pd.to_datetime(idp_date, format='%Y-%m-%d'))
     clean_idp['Time Period'] = pd.to_datetime(clean_idp['Time Period'],
-                                              format='%I:%M %p').dt.time
+                                              format='%H:%M:%S').dt.time
 
     return clean_idp
 
 
-def xlwb_csvs(infile, create_files=True):
-    """xlwb_csvs(infile)
-        -split excel workbook tabs into individual csv files
-        -also returns the dictionary of pandas objects
-        -set create_files=False to bypass creating files
+def cleanSEG(infile):
+    """cleanSEG
+        -cleans segment data for off phone activity reporting
+        -returns clean pandas object with necessary data
     """
-    dict_pdobj = pd.read_excel(infile, sheet_name=None)
-    if create_files:
-        for tabdate in dict_pdobj.keys():
-            dict_pdobj[tabdate].to_csv(f'IDP_{tabdate}.csv', index=False)
-
-    return dict_pdobj
+    pass
